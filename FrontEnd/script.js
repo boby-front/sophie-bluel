bcc661fb7420bc6e03015fd8414578fd101174b6
boby-front <delanglebob38@gmail.com>
2023-05-15 08:33:40 +0200

    add last version

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -48,12 +48,21 @@ fetch("http://localhost:5678/api/works")
 
 // **********  2. FILTRAGE DES IMAGES PAR CATEGORIE. ************
 
-// On récupère des catégories à partir d'une API et crée des boutons de filtrage pour afficher
-// des éléments de galerie selon la catégorie choisie. Il commence par définir un tableau
-// allCategories avec une catégorie "Tous" ayant un ID de -1. Ensuite, il récupère les catégories
-// via l'API, les ajoute au tableau, et crée des boutons de filtrage pour chaque catégorie.
-// Les boutons sont ajoutés à l'élément HTML avec l'ID "blocFilter", et lorsqu'un bouton est cliqué,
-// les éléments de la galerie sont affichés ou masqués en fonction de la catégorie sélectionnée.
+// Les images sont filtrées en fonction des catégories sélectionnées.
+
+// - "allCategories" est initialisée avec un tableau contenant une catégorie spéciale "Tous".
+// - Une requête est effectuée à l'URL  pour obtenir la liste des catégories disponibles.
+// - La réponse de la requête est traitée en convertissant la réponse en JSON.
+// - Pour chaque catégorie obtenue, l'objet catégorie est ajouté au tableau allCategories.
+// HTML avec les options de catégorie.
+// - L'élément du DOM avec l'ID "blocFilter" est sélectionné.
+// - Pour chaque catégorie dans allCategories, un bouton est créé et configuré avec les
+// attributs appropriés, tels que la classe, l'ID et le texte.
+// - Si la catégorie a un ID de -1, une classe supplémentaire "fontButton2" est ajoutée au bouton.
+// - Le bouton est ajouté à l'élément du DOM avec l'ID "blocFilter"
+// - Un écouteur d'événement est attaché à chaque bouton pour gérer le clic de l'utilisateur.
+// - Lorsqu'un bouton est cliqué, tous les boutons du filtre sont réinitialisés en supprimant la classe "fontButton2" et le bouton cliqué reçoit la classe "fontButton2".
+// - Ensuite, tous les éléments de la galerie d'images ayant la classe "gallery-item" sont parcourus.
 
 let allCategories = [
   {
@@ -151,6 +160,19 @@ manageDisplay();
 
 // ************  4. GESTIONS DE CLICK // TRAVAUX  MODALE ***************
 
+// - Sélection des éléments DOM nécessaires avec des sélecteurs.
+// - Attachement d'un écouteur d'événement à chaque élément  pour exécuter une fonction associée au clic.
+// - Vidage du contenu de l'élément avec l'ID "gallery-modal".
+// - Configuration de l'affichage ou de la masquage d'éléments de l'interface utilisateur via les
+// éléments "modalWrapper" et "modalAddPhoto".
+// - Création et configuration d'éléments DOM pour chaque élément du tableau allProjects.
+// - Attachement d'un écouteur d'événement à l'icône de corbeille pour afficher une boîte de
+// confirmation de suppression.
+// - Attachement d'un écouteur d'événement à l'icône de la flèche gauche pour configurer l'affichage
+// des éléments de l'interface utilisateur.
+// - Ajout du contenu généré à l'élément avec l'ID "gallery-modal".
+// - Affichage de la modalité de la galerie en définissant la propriété display sur "flex".
+
 const galleryModal = document.getElementById("modal1");
 const galleryModalContent = galleryModal.querySelector("#gallery-modal");
 const closeModal = document.querySelectorAll(".close-modal");
@@ -181,6 +203,7 @@ document.querySelectorAll(".editorModeP").forEach((link) => {
       icon.setAttribute("class", "fa-solid fa-trash-can");
       divTrash.appendChild(icon);
       imgContainer.appendChild(divTrash);
+
       divTrash.addEventListener("click", function (Event) {
         const confirmDelete = window.confirm(
           "Êtes-vous sûr de vouloir supprimer cette image ?"
@@ -215,18 +238,11 @@ galleryModal.addEventListener("click", (e) => {
   }
 });
 
-// On ajoute un écouteur d'événement pour la fermeture de la fenêtre modale. Lorsqu'on
-// clique sur le bouton de fermeture la fenêtre modale est cachée.
-
 closeModal.forEach((element) => {
   element.addEventListener("click", () => {
     galleryModal.style.display = "none";
   });
 });
-
-// On ajoute un écouteur d'événement pour le bouton "Ajouter une photo" dans la fenêtre modale.
-// Lorsque l'utilisateur clique sur le bouton, la fenêtre modale de la galerie est cachée et
-// la fenêtre modale d'ajout de photo est affichée.
 
 boutonModal.addEventListener("click", (e) => {
   e.preventDefault();
@@ -234,6 +250,13 @@ boutonModal.addEventListener("click", (e) => {
   modalAddPhoto.style.display = "block";
 });
 // ************  SUPRESSION DES IMAGES DU DOM DEPUIS L'API ***************
+
+// La fonction deleteItem(imageId) envoie une requête DELETE à l'URL spécifiée avec l'ID de l'image
+//  à supprimer, en incluant l'autorisation d'accès. Elle vérifie la réponse de la requête pour
+// s'assurer que l'élément a été supprimé avec succès. Si tel est le cas, elle met à jour allProjects
+// en filtrant l'élément correspondant, et supprime l'élément de la galerie et du modal en utilisant
+// les sélecteurs appropriés. Si la suppression échoue, une erreur est générée et une alerte est affichée
+// à l'utilisateur.
 
 function deleteItem(imageId) {
   fetch(`http://localhost:5678/api/works/${imageId}`, {
@@ -269,6 +292,10 @@ function deleteItem(imageId) {
 }
 //************  AJOUTER UNE IMAGE COMME TRAVAUX *****************/
 
+// On créé une fonction qui permet de remplir un élément de sélection HTML avec des options générées
+// à partir d'un tableau d'objets de catégories. Chaque option a une valeur et un texte
+// correspondant à l'id et au nom de la catégorie.
+
 function categorySelect(categories) {
   const categorySelect = document.getElementById("category");
   categories.forEach((category) => {
@@ -279,11 +306,14 @@ function categorySelect(categories) {
   });
 }
 
+// Ce bloc déclare les variables et constantes nécessaires pour les éléments et les données
+// utilisées dans le reste du code. Il crée également un élément <input> de type "file"
+// qui est ajouté au formulaire de téléchargement.
+
 const buttonAddPhoto = document.querySelector(".buttonAddPhoto");
 const uploadForm = document.getElementById("uploadForm");
 const buttonValidate = document.querySelector(".buttonValidate");
 
-// Création d'un seul élément input et ajout de l'écouteur d'événements "change"
 const input = document.createElement("input");
 input.type = "file";
 input.name = "image";
@@ -291,13 +321,22 @@ input.accept = "image/*";
 input.style.display = "none";
 uploadForm.appendChild(input);
 
-let file; // déplacez 'file' ici pour le rendre accessible dans le reste du code
+let file;
+
+// Cet événement se déclenche lorsque l'utilisateur sélectionne un fichier à télécharger.
+// Il récupère le premier fichier sélectionné et effectue des actions pour mettre à jour
+// l'interface utilisateur en conséquence.
 
 input.addEventListener("change", () => {
   file = input.files[0];
   const modal = document.querySelector(".modal-addPhoto");
   const titleInput = modal.querySelector("#title");
   const categorySelect = modal.querySelector("#category");
+
+  // On active ou désactive le bouton de validation en fonction de l'état des champs titleInput
+  // et categorySelect. La fonction  définit la couleur de fond du bouton
+  // de validation en vert  si les champs sont valides (non vides pour titleInput
+  // et différent de "default" pour categorySelect), sinon elle le réinitialise.
 
   function enableValidateButton() {
     buttonValidate.style.backgroundColor =
@@ -309,7 +348,8 @@ input.addEventListener("change", () => {
   titleInput.addEventListener("input", enableValidateButton);
   categorySelect.addEventListener("input", enableValidateButton);
 
-  // On affiche l'image selectionné et masque le paragraphe et le boutton.
+  // On affiche l'image sélectionné et retire le logo et boutton de recherche d'ajout de fichier.
+
   const img = document.createElement("img");
   img.id = "upload-image";
   img.src = URL.createObjectURL(file);
@@ -319,19 +359,26 @@ input.addEventListener("change", () => {
   divAddPhoto.querySelector("button").style.display = "none";
 });
 
+// L'événement de clic écoute le bouton "buttonValidate", envoie les données du formulaire
+// via une requête POST, traite la réponse JSON et effectue des actions sur l'interface utilisateur.
+
+// - Il empêche le comportement par défaut de l'événement de clic.
+// - Il sélectionne des éléments du document.
+// - Il crée un objet FormData pour stocker les données du formulaire.
+// - Il envoie une requête POST à une URL spécifique avec les données du formulaire.
+// - Il traite la réponse JSON obtenue.
+// - Il effectue des modifications sur l'interface utilisateur en fonction de la réponse.
+// - Il gère les erreurs éventuelles.
+
 buttonValidate.addEventListener("click", (event) => {
-  // déplacez l'écouteur d'événements 'click' ici
   event.preventDefault();
   const modal = document.querySelector(".modal-addPhoto");
   const titleInput = modal.querySelector("#title");
   const categorySelect = modal.querySelector("#category");
-
   const formData = new FormData();
   formData.append("image", file);
   formData.append("title", titleInput.value);
   formData.append("category", categorySelect.value);
-
-  // votre code fetch ici...
 
   fetch("http://localhost:5678/api/works", {
     method: "POST",
@@ -366,6 +413,8 @@ buttonValidate.addEventListener("click", (event) => {
     })
     .catch((error) => console.error(error));
 });
+
+// On simule le clic pour ouvrir la fenetre de recherche du fichier a telecharger.
 
 buttonAddPhoto.addEventListener("click", () => {
   input.click();
